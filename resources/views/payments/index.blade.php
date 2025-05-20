d868cafda5f3a4e240667bae3c2cf68f60cd2f14
unknown <143472053+AthaillahAsyraf@users.noreply.github.com>
2025-05-20 23:26:53 +0700

    part 12

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -1,24 +1,54 @@
 @extends('layout')
 @section('content')
+
+                <div class="card">
+                    <div class="card-header">
+                        <h2>Payment</h2>
+                    </div>
+                    <div class="card-body">
+                        <a href="{{ url('/payments/create') }}" class="btn btn-success btn-sm" title="Add New payment">
+                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
+                        </a>
+                        <br/>
+                        <br/>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Enrollment No</th>
+                                        <th>Paid Date</th>
+                                        <th>Amount</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($payments as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->enrollment->enroll_no }}</td>
+                                        <td>{{ $item->paid_date }}</td>
+                                        <td>{{ $item->amount }}</td>
  
-<div class="card">
-  <div class="card-header">Edit Page</div>
-  <div class="card-body">
-      
-      <form action="{{ url('batches/' .$batches->id) }}" method="post">
-        {!! csrf_field() !!}
-        @method("PATCH")
-        <input type="hidden" name="id" id="id" value="{{$batches->id}}" id="id" />
-        <label>Name</label></br>
-        <input type="text" name="name" id="name" value="{{$batches->name}}" class="form-control"></br>
-        <label>Course</label></br>
-        <input type="text" name="course_id" id="course_id" value="{{$batches->course->name}}" class="form-control"></br>
-        <label>Start Date</label></br>
-        <input type="text" name="start_date" id="start_date" value="{{$batches->start_date}}" class="form-control"></br>
-        <input type="submit" value="Update" class="btn btn-success"></br>
-    </form>
-   
-  </div>
-</div>
+                                        <td>
+                                            <a href="{{ url('/payments/' . $item->id) }}" title="View batch"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
+                                            <a href="{{ url('/payments/' . $item->id . '/edit') }}" title="Edit batche"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
  
-@stop
+                                            <form method="POST" action="{{ url('/payments' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                {{ method_field('DELETE') }}
+                                                {{ csrf_field() }}
+                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete payments" onclick="return confirm('Confirm delete?')">
+                                                    <i class="fa fa-trash-o" aria-hidden="true"></i> Delete
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>    
+                            </table>
+                        </div>
+ 
+                    </div>
+                </div>
+
+@endsection
