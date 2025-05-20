76e2561193ec4b18d7a44aa4a5ee3f8bf8af607b
unknown <143472053+AthaillahAsyraf@users.noreply.github.com>
2025-05-20 20:45:58 +0700

    part 7

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BatchController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\CourseController;
@@ -21,5 +22,7 @@ Route::get('/', function () {
     return view('Layout');
 });
 Route::resource('/students', StudentController::class);
+
 Route::resource('/teachers', TeacherController::class);
 Route::resource('/courses', CourseController::class);
+Route::resource('/batches', BatchController::class);
